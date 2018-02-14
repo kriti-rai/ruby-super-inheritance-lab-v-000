cb58f076af21b4ea59816262e955b79da91ce988
Kriti Rai <kriti.rai@gmail.com>
2018-02-14 19:59:40 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require_relative "./student.rb"
 class ChattyStudent < Student
   def hello
@@ -6,10 +5,6 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   def raise_hand
-    super
-    binding.pry
-    # 10.times do
-    #   puts "Pick me!"
-    # end
-  end
+    10.times do super end
+      puts "Pick me!"
 end
